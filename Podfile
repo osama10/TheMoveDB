f4438cd509a1f54838bcd07909455cfda38b5aa2
Osama Bin Bashir <osama.bashir@zalando.de>
2019-11-25 16:37:29 +0100

    added Movie Response

--- Podfile
+++ Podfile
@@ -4,10 +4,9 @@ target 'TheMovieDB' do
   use_frameworks!
 
   pod 'OBNetworking', :path => '../OBNetworking'
-  
+
   target 'TheMovieDBTests' do
     inherit! :search_paths
-    # Pods for testing
   end
 
 end
---
+++ TheMovieDB/Features/Models/MovieResponse.swift
@@ -0,0 +1,23 @@
+//
+//  MovieResponse.swift
+//  TheMovieDB
+//
+//  Created by Osama Bashir on 11/24/19.
+//  Copyright © 2019 Osama Bashir. All rights reserved.
+//
+
+import Foundation
+
+struct MovieResponseModel: Decodable {
+    let page: Int?
+    let totalResults: Int?
+    let totalPages: Int?
+    let movies: [Movie]?
+
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case movies = "results"
+    }
+}
