8469fdb2752bf26450e726dfedfa3895571f7178
Osama Bin Bashir <osama.bashir@zalando.de>
2019-12-01 13:47:34 +0100

    changes pushed

---
+++ TheMovieDBTests/UITests/MovieList/Mocks/MockMovieListViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  MockMovieListViewModel.swift
+//  TheMovieDBTests
+//
+//  Created by Osama Bashir on 12/1/19.
+//  Copyright © 2019 Osama Bashir. All rights reserved.
+//
+
+import Foundation
+@testable import TheMovieDB
+
+final class MockMovieListViewModel: MoviesListViewModelProtocol {
+    var delegate: MoviesListViewModelDelegate?
+    var numberOfRows: Int = 0
+    var sections: Int = 0
+    var title: String = "TheMovieDB"
+    var isLast: Bool = false
+    private let movieListCellViewModel: [MovieListCellViewModel]
+    init(movieListCellViewModel: [MovieListCellViewModel]) {
+        self.movieListCellViewModel = movieListCellViewModel
+    }
+
+    func viewDidLoad() {
+        delegate?.reloadData()
+    }
+
+    func didTapOnCell(index: Int) {
+
+    }
+
+    func getMovieListCellViewModel(for index: Int) -> MovieListCellViewModel {
+        return movieListCellViewModel[index]
+    }
+
+
+}
--- TheMovieDBTests/UITests/MovieList/Test/MovieListsSnapshotTests.swift
+++ TheMovieDBTests/UITests/MovieList/Test/MovieListsSnapshotTests.swift
@@ -6,28 +6,26 @@
 //  Copyright © 2019 Osama Bashir. All rights reserved.
 //
 
-import XCTest
+import FBSnapshotTestCase
+@testable import TheMovieDB
 
-class MovieListsSnapshotTests: XCTestCase {
+class MovieListsSnapshotTests: FBSnapshotTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        recordMode = true
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let storyboard = UIStoryboard(storyboard: .movies)
+        let movieDetailController: MovieListViewController = storyboard.instantiateViewController()
+        
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
+}
+
+extension MovieListsSnapshotTests {
+    private func getMovieListCellViewModel() -> [MovieListCell]
 }
